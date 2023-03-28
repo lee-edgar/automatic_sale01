f9487c5f0f319201928c4b9545edd5d7774646d7
lee-edgar <61241244+lee-edgar@users.noreply.github.com>
2023-03-28 20:38:50 +0900

    Update pyqt5.py

--- app/pyqt5.py
+++ app/pyqt5.py
@@ -4,12 +4,10 @@ from PyQt5.QtWidgets import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setGeometry(100, 200, 300, 400)#window size
 
 
 app = QApplication(sys.argv)
-
-
 window = MyWindow()
 window.show()
-
 app.exec_()
